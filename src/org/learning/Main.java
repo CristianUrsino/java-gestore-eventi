b109cbf2cd612533f0c2760c1704a1126d6c3626
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-09 11:39:07 +0200

    updated Main with Concert test

--- src/org/learning/Main.java
+++ src/org/learning/Main.java
@@ -1,6 +1,8 @@
 package org.learning;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,5 +15,13 @@ public class Main {
         System.out.println(e.getReservedSeats());
         e.cancelReservations(30);
         System.out.println(e.getReservedSeats());
+
+        LocalTime time = LocalTime.of(16,30,10);
+        Concert c = new Concert("concerto",date,1000, time, new BigDecimal("30.5021251141"));
+        c.reservationSeats( 500);
+        System.out.println("\n\n"+c.getReservedSeats());
+        c.cancelReservations(400);
+        System.out.println(c.getReservedSeats());
+        System.out.println(c.toString());
     }
 }
